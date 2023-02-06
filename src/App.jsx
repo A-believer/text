b94cc64587356fa5af5502ffd27aa5b685acc158
davedev <davidabolade29@gmail.com>
2023-02-06 16:23:27 +0100

    folder structure complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {Box} from "@mui/material";
+import {ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed} from "./components"
 function App() {
 
 
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail/>} />
+          <Route path='/channel/:id' element={<VideoDetail/>} />
+          <Route path='/search/:searchTerm' element={<SearchFeed/>} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
 }
 
