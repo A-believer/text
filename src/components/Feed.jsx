90e822c47ae7d8b23a3653a0ae1b6af019f67edb
davedev <davidabolade29@gmail.com>
2023-02-06 18:44:07 +0100

    Navbar and Sidebar completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Box, Stack, Typography } from "@mui/material"
+// import { fetchFromAPI } from "../utils/fetchFromAPI"
+import { Videos, Sidebar} from "./"
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+  }, [selectedCategory])
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: {sx: "column", md: "row"}}}>
+      <Box sx={{ height: {sx: "auto", md: "92vh"}, borderRight: "1px solid #3d3d3d", px: {sx: 0, md: 2}}}>
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={ setSelectedCategory} />
+      </Box>
+    </Stack>
   )
 }
 
